6cf7cb02e193676e742c1261502c4e879db774c0
STUDENT Kiefer E. Boakye Yiadom <Kiefer.E.BoakyeYiadom@students.ittralee.ie>
2024-10-02 12:52:02 +0100

    Signed-off-by: STUDENT Kiefer E. Boakye Yiadom <Kiefer.E.BoakyeYiadom@students.ittralee.ie>

--- Radz_Kief/Assets/MyHero.cs
+++ Radz_Kief/Assets/MyHero.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class MyHero : MonoBehaviour
 {
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+      animator =  GetComponent<Animator>();  
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
-            transform.position += transform.forward * Time.deltaTime;
 
+        {
+            transform.position += transform.forward * Time.deltaTime;
+            animator.SetBool("Is Running", true);
+
+
+        }
         if (Input.GetKey(KeyCode.A))
             transform.Rotate(Vector3.up, 120*Time.deltaTime);
 
         if (Input.GetKey(KeyCode.S))
-            transform.position += Vector3.back*Time.deltaTime;
+            transform.position -= transform.forward*Time.deltaTime;
 
         if (Input.GetKey(KeyCode.D))
             transform.Rotate(Vector3.down, 120*Time.deltaTime);
